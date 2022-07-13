c230a95dd4c4c0f562dc46d8e7376fabd0847478
Muhammad Hammad Shahid <hammad.shahid.pk@gmail.com>
2022-07-14 00:04:36 +0500

    contracts, configs and miscs

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x7771891dDC4bc6B2fc251560aA8cBD41e44D18B3";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
